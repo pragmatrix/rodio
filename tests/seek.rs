550c2771c471f725c1a9f37097cdf4573fa48ef5
Armin Sander <armin@replicator.org>
2025-05-22 20:41:44 +0200

    cs: fmt

--- tests/seek.rs
+++ tests/seek.rs
@@ -175,8 +175,7 @@ fn seek_does_not_break_channel_order(
     }
 }
 
-fn second_channel_beep_range<R: rodio::Source>(source: &mut R) -> std::ops::Range<usize>
-{
+fn second_channel_beep_range<R: rodio::Source>(source: &mut R) -> std::ops::Range<usize> {
     let channels = source.channels() as usize;
     let samples: Vec<f32> = source.by_ref().collect();
 
